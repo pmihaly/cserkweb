67f9ed97275c87fdf8c4ee93e26033a3e006b283
Papp Mihály <papp.misi@protonmail.com>
2019-10-14 17:46:13 +0200

    Képfeltöltés

--- cserkweb/urls.py
+++ cserkweb/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from blog import urls as blog_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("bejegyzesek/", include(blog_urls), name="blog"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
