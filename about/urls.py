2beb81753565e4b71cf81d3925eae11e414df1aa
Papp Mihály <papp.misi@protonmail.com>
2019-10-23 08:42:58 +0200

    Rólunk oldal

--- about/urls.py
+++ about/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import Homepage
+from .views import Homepage, AboutUs
 
 app_name = "about"
 
-urlpatterns = [path("", Homepage.as_view(), name="homepage")]
+urlpatterns = [
+    path("", Homepage.as_view(), name="homepage"),
+    path("rolunk/", AboutUs.as_view(), name="about_us"),
+]
 
--- about/views.py
+++ about/views.py
@@ -6,3 +6,7 @@ from django.views.generic.base import TemplateView
 
 class Homepage(TemplateView):
     template_name = "about/homepage.html"
+
+
+class AboutUs(TemplateView):
+    template_name = "about/about_us.html"
