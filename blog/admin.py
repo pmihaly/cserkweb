a776fdc9f650bbe4a43717c920edb8e83eda19c3
pmisi <papp.misi@protonmail.com>
2019-11-09 20:11:23 +0100

    Hírek model

--- blog/admin.py
+++ blog/admin.py
@@ -6,6 +6,7 @@ from . import models, forms
 
 @admin.register(models.Announcement)
 class AnnouncementAdmin(admin.ModelAdmin):
+    model = models.Announcement
     list_display = ["title", "author", "published", "created_at", "updated_at"]
     list_editable = ["published"]
     search_fields = ["title", "text"]
@@ -16,3 +17,16 @@ class AnnouncementAdmin(admin.ModelAdmin):
             obj.author = request.user
         super().save_model(request, obj, form, change)
 
+
+@admin.register(models.Event)
+class EventAdmin(admin.ModelAdmin):
+    model = models.Event
+    list_display = ["title", "author", "published", "created_at", "updated_at"]
+    list_editable = ["published"]
+    search_fields = ["title", "text"]
+    form = forms.PostForm
+
+    def save_model(self, request, obj, form, change):
+        if not hasattr(obj, "author"):
+            obj.author = request.user
+        super().save_model(request, obj, form, change)
