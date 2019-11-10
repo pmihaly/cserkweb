caf649e768bc42dcde24a138a946c8f633023858
Papp Mihály <papp.misi@protonmail.com>
2019-11-10 08:22:47 +0100

    List vonatkozzon minden bejegyzésre, külön-külön Detail

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,55 @@
+from itertools import chain
+
 from django.shortcuts import render
-from .models import Announcement
-from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+from .models import Announcement, Event
 
 # Create your views here.
 
 
 class PostList(ListView):
-    model = Announcement
-    template_name = "blog/bejegyzes_list.html"
-    queryset = Announcement.get_published().order_by("-updated_at")
+    template_name = "blog/post_list.html"
     paginate_by = 9
 
+    def get_queryset(self):
+        return list(
+            chain(
+                Announcement.get_published().order_by("-updated_at"),
+                Event.get_published().order_by("-updated_at"),
+            )
+        )
 
-class PostDetail(DetailView):
+
+class AnnouncementDetail(DetailView):
     model = Announcement
     template_name = "blog/bejegyzes_detail.html"
 
     def get_context_data(self, **kwargs):
 
-        context = super(PostDetail, self).get_context_data(**kwargs)
+        context = super(AnnouncementDetail, self).get_context_data(**kwargs)
         context["domain_name"] = (
             "https://"
             if self.request.is_secure()
             else "http://" + self.request.get_host()
         )
+
+        return context
+
+
+class EventDetail(DetailView):
+    model = Event
+    template_name = "blog/bejegyzes_detail.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super(EventDetail, self).get_context_data(**kwargs)
+        context["domain_name"] = (
+            "https://"
+            if self.request.is_secure()
+            else "http://" + self.request.get_host()
+        )
+        context["event"] = True
 
         return context
