46091e73d1633248455943fd8003e0c9134c3be7
Papp Mihály <papp.misi@protonmail.com>
2019-11-30 09:04:09 +0100

    Teszt refaktor

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -10,35 +10,35 @@ from . import InitPosts
 
 
 class TestModels(InitPosts):
-    def test_post_creation(self):
-        self.assertTrue(isinstance(self.post, Announcement))
+    def test_announcement_creation(self):
+        self.assertTrue(isinstance(self.announcement, Announcement))
 
-        self.assertEqual(self.post.__unicode__(), self.post.title)
-        self.assertEqual(self.post.__str__(), self.post.title)
+        self.assertEqual(self.announcement.__unicode__(), self.announcement.title)
+        self.assertEqual(self.announcement.__str__(), self.announcement.title)
 
-        self.assertEqual(self.post.slug, slugify(self.post.title))
+        self.assertEqual(self.announcement.slug, slugify(self.announcement.title))
 
     def test_update(self):
-        updated_post, _ = Announcement.objects.update_or_create(
-            title=self.post.title, defaults={"title": "yoyoyo"}
+        updated_announcement, _ = Announcement.objects.update_or_create(
+            title=self.announcement.title, defaults={"title": "yoyoyo"}
         )
 
-        self.assertNotEqual(updated_post.slug, self.post.slug)
-        self.assertEqual(self.post.slug, slugify(self.post.title))
+        self.assertNotEqual(updated_announcement.slug, self.announcement.slug)
+        self.assertEqual(self.announcement.slug, slugify(self.announcement.title))
 
     def test_get_absolute_url(self):
         absolute_url = reverse(
-            "blog:announcement-detail", kwargs={"slug": self.post.slug}
+            "blog:announcement-detail", kwargs={"slug": self.announcement.slug}
         )
-        self.assertEqual(self.post.get_absolute_url(), absolute_url)
+        self.assertEqual(self.announcement.get_absolute_url(), absolute_url)
 
     def test_get_other(self):
-        get_other = self.post.get_other()
+        get_other = self.announcement.get_other()
 
         self.assertEqual(len(get_other), 9)
 
         self.assertGreaterEqual(get_other[0].updated_at, list(get_other)[-1].updated_at)
 
-        self.assertNotIn(self.post, get_other)
+        self.assertNotIn(self.announcement, get_other)
 
-        self.assertNotIn(self.related, get_other)
+        self.assertNotIn(self.related_announcements, get_other)
