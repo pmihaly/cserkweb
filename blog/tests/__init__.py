ef970bf7aa830677885fb16e598a8983689018f9
Papp Mihály <papp.misi@protonmail.com>
2019-11-30 09:04:09 +0100

    Teszt refaktor

--- blog/tests/__init__.py
+++ blog/tests/__init__.py
@@ -2,26 +2,48 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from model_bakery import baker
 
-from blog.models import Announcement
+from blog.models import Announcement, Event
 
 
 class InitPosts(TestCase):
+
+    text = "<h1>test test</h1>"
+    author = baker.make(User)
+
+    def setup_test_object_no_relation(self, model, **model_kwargs):
+
+        # pylint: disable=unused-variable
+        other_objects = baker.make(model, _quantity=30, text=self.text, image="")
+
+        test_object = baker.make(
+            model, make_m2m=True, text=self.text, image="", **model_kwargs
+        )
+
+        object_with_image = baker.make(
+            model, text=self.text, image="cover_images/pic.jpg"
+        )
+
+        return (test_object, object_with_image)
+
+    def setup_test_object_relation(self, model, **model_kwargs):
+
+        related_objects = baker.prepare(
+            model,
+            _quantity=4,
+            text=self.text,
+            author=self.author,
+            image="",
+            **model_kwargs
+        )
+
+        return (
+            *self.setup_test_object_no_relation(model, related=related_objects),
+            related_objects,
+        )
+
     def setUp(self):
-        self.text = "<h1>test test</h1>"
-        self.author = baker.make(User)
-
-        self.related = baker.prepare(
-            Announcement, _quantity=4, text=self.text, author=self.author, image=""
-        )
-
-        self.get_other = baker.make(
-            Announcement, _quantity=30, text=self.text, image=""
-        )
-
-        self.post = baker.make(
-            Announcement, make_m2m=True, text=self.text, related=self.related, image="",
-        )
-
-        self.post_with_image = baker.make(
-            Announcement, text=self.text, image="cover_images/pic.jpg"
-        )
+        (
+            self.announcement,
+            self.announcement_with_image,
+            self.related_announcements,
+        ) = self.setup_test_object_relation(Announcement)
--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -5,26 +5,34 @@ from . import InitPosts
 
 
 class TestViews(InitPosts):
-    def test_post_list_view(self):
+    def test_announcement_list_view(self):
         url = reverse("blog:announcement-list")
         res = self.client.get(url)
 
-        self.assertContains(res, self.post.title, status_code=200)
+        self.assertContains(res, self.announcement.title, status_code=200)
 
-    def test_post_list_view_with_image(self):
+    def test_announcement_list_view_with_image(self):
         url = reverse("blog:announcement-list")
         res = self.client.get(url)
 
-        self.assertContains(res, self.post_with_image.image.url, status_code=200)
+        self.assertContains(
+            res, self.announcement_with_image.image.url, status_code=200
+        )
 
-    def test_post_detail_view(self):
-        url = reverse("blog:announcement-detail", kwargs={"slug": self.post.slug})
+    def test_announcement_detail_view(self):
+        url = reverse(
+            "blog:announcement-detail", kwargs={"slug": self.announcement.slug}
+        )
         res = self.client.get(url)
 
-        self.assertContains(res, self.post.title, status_code=200)
+        self.assertContains(res, self.announcement.title, status_code=200)
 
-    def test_post_detail_view_with_image(self):
-        url = reverse("blog:announcement-detail", kwargs={"slug": self.post.slug})
+    def test_announcement_detail_view_with_image(self):
+        url = reverse(
+            "blog:announcement-detail", kwargs={"slug": self.announcement.slug}
+        )
         res = self.client.get(url)
 
-        self.assertContains(res, self.post_with_image.image.url, status_code=200)
+        self.assertContains(
+            res, self.announcement_with_image.image.url, status_code=200
+        )
